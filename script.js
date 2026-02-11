5fff95ebd3beeb5909cb2dbb060a84a6465b3284
Lilian Fevier <lilianfervier@gmail.com>
2026-02-11 10:30:36 -0300

    script.js

--- script.js
+++ script.js
@@ -303,7 +303,7 @@ let selecionados = [];
 secoes.forEach(secao => {
 
   const details = document.createElement("details");
-  details.open = true;
+  details.open = false;
 
   const summary = document.createElement("summary");
   summary.textContent = secao.titulo;
